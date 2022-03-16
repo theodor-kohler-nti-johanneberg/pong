5fb083b7e926584dcde13cd237a09d063d8ae523
theodor-kohler-nti-johanneberg <94974723+theodor-kohler-nti-johanneberg@users.noreply.github.com>
2022-03-16 22:13:49 +0100

    Made physics engine and started canvas

--- main.js
+++ main.js
@@ -1,19 +1,58 @@
-/*UPPLÖSNING 1366x768*/ 
+/*UPPLÖSNING 1366x768*/
+/*margin(38,35px) racket(38,35px) margin(613,6px) racket(38,35px) margin(38,35px)*/
+/*Ball = 7,67px*7,67px */
 
+let width = 1365;
+let height = 767;
+let ballLength = 7.67;
+let racketLength = 460.2;
 let racket1Y = 153.4;
 let racket2Y = 153.4;
-let ballX = 682.5;
-let ballY = 383.5;
-let hVelocity = 1.0;
-let vVelocity = 0.0;
 let score1 = 0;
 let score2 = 0;
 
-while (true) {
-    ballX += hVelocity;
-    ballY += vVelocity;
+let ball = {
+    x: 682.5,
+    y: 383.5,
+    velocityX: 1.0,
+    velocityY: 0.0,
+};
 
-    switch (checkWinner(ballX)) {
+function checkWinner(x) {
+    if (x >= width) {
+        return 1;
+    } else if (x <= 0) {
+        return 2;
+    }
+
+    return 0;
+}
+
+function newVelocities(ball) {
+    if (ball.x <= 0.1 * width + ballLength && ball.y >= racket1Y && ball.y <= racket1Y + racketLength || ball.x >= width - (0.1 * width + ballLength) && ball.y >= racket2Y && ball.y <= racket2Y + racketLength || ball.y == height || ball.y == 0) {
+        ball.velocityX = -ball.velocityX;
+        ball.velocityY = -ball.velocityY;
+
+        return ball;
+    }
+
+    return ball;
+}
+
+function draw() {
+    let canvas = document.getElementById("canvas");
+    let racket1 = canvas.getContext("2d");
+    racket1.beginPath();
+    racket1.rect(0.05 * width, racket1Y, 0.1 * width, 0.6 * height)
+}
+
+function app() {
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+
+    ball = newVelocities(ball);
+
+    switch (checkWinner(ball.x)) {
         case 1:
             //Player 1 won
             score1++;
@@ -25,20 +64,9 @@ while (true) {
             break;
     }
 
-
-
+    draw();
 }
 
-function checkWinner(ballX) {
-    if (ballX >= 1365) {
-        return 1;
-    } else if (ballX <= 0) {
-        return 2;
-    }
-
-    return 0;
-}
-
-function newVelocities(hVelocity, vVelocity) {
-    
+while (true) {
+    setTimeout(app(), 100)
 }
